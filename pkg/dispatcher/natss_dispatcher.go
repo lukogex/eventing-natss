b572cde2d9d2e0f192f3264cc9095fbd948012ae
Lukas Kranabetter <l.kranabetter@netconomy.net>
2022-04-28 10:57:26 +0200

    feat(XXXL-29303): Fix merge conflicts.

--- pkg/dispatcher/natss_dispatcher.go
+++ pkg/dispatcher/natss_dispatcher.go
@@ -433,4 +433,5 @@ func (s *subscriptionsSupervisor) getChannelReferenceFromHost(host string) (even
 		return cr, fmt.Errorf("Invalid HostName:%q. HostName not found in any of the watched natss channels", host)
 	}
 	return cr, nil
+	i
 }
